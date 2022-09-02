d8fe45be93034d40d366533cea68bf45ca160e41
caropinto <carol.pinto.lopez@gmail.com>
2022-09-01 21:37:14 -0500

    Migraciones de cuentas y categorias

--- database/migrations/2022_09_02_021001_create_cuentas_table.php
+++ database/migrations/2022_09_02_021001_create_cuentas_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cuentas', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('saldo')->default(0);
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cuentas');
     }
 };
